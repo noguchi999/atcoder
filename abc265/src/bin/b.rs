5c5c24c4a868f62f54ea73add24672d053ff4cbe
Osamu Noguchi <noguchi@headwaters.co.jp>
2024-11-23 09:55:00 +0900

    [modify] abc265

--- abc265/src/bin/b.rs
+++ abc265/src/bin/b.rs
@@ -7,4 +7,11 @@ fn main() {
 
     let mut a = [0; 3];
     let mut bonus = [0; n + 1];
+    for _ in 0..m {
+        proconio::input! {
+            x: i32,
+            y: i32,
+        };
+        bonus[x] = y;
+    }
 }
