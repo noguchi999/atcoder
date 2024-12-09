29a7004216a84c2f57333cf4ab3260bd05d0c071
Osamu Noguchi <noguchi@headwaters.co.jp>
2024-12-09 15:12:00 +0900

    [modify] abc265

--- abc265/src/bin/c.rs
+++ abc265/src/bin/c.rs
@@ -18,7 +18,7 @@ fn main() {
 
     for i in 0..(10_i32.pow(10)) {
         if visited[now_g][now_r] == 1 {
-            println!("-2");
+            println!("-");
             return;
         }
     }
