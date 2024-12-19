329205834ffbd17ac776a061c7fb871091ce1c30
Osamu Noguchi <noguchi@headwaters.co.jp>
2024-12-19 16:42:01 +0900

    [modify] abc265

--- abc265/src/bin/c.rs
+++ abc265/src/bin/c.rs
@@ -33,5 +33,14 @@ fn main() {
                 std::process::exit(0);
             }
         }
+
+        if g[now_g].chars().nth(now_r) == Some('R') {
+            if now_g < w - 2 {
+                now_g += 1;
+            } else {
+                println!("{}, {}", now_g + 1, now_r + 1);
+                std::process::exit(0);
+            }
+        }
     }
 }
