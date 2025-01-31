75f30c5d3e5635ed9c53f4a6bc2b064500f66e47
Osamu Noguchi <noguchi@headwaters.co.jp>
2025-01-31 16:39:58 +0900

    [modify] comment

--- src/beam_search_02.rs
+++ src/beam_search_02.rs
@@ -23,25 +23,18 @@ fn beam_search_action_with_time_threshold(
 }
 
 fn main() {
-    /********************************************************************/
-    // 1. Read input
-    let all_boards = read(); // The read information is stored as 100 map information in the global area all_boards
-    /********************************************************************/
-    // 2. Select and output K maps
-    // For simplicity, select K maps in the order read from the input
+    let all_boards = read();
+
     let mut state = State::new();
     for i in 0..K {
         state.boards[i] = all_boards[i];
         print!("{} ", i);
     }
     println!();
-    /********************************************************************/
-    // 3. Perform beam search with width 64 for 3 seconds
+
     let actions = beam_search_action_with_time_threshold(&state, 64, 3000);
-    /********************************************************************/
-    // 4. Output the actions of the search results
     for action in actions {
-        print!("{}", action); // Replace `action` with the appropriate output format
+        print!("{}", action);
     }
     println!();
 }
